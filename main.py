50ed4e36dca7730aa79e58aae412dacec30324c3
ngeorgiev92 <ngeorgiev92@gmail.com>
2025-03-12 13:27:07 +0000

    word count function added, main now prints word count for frankenstein.txt

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ def get_book_text(filedir):
         text = f.read()
     return text
 
+def count_words(filedir):
+    with open(filedir) as f:
+        text = f.read()
+    word_arr = text.split()
+    return len(word_arr)
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    print(f"{count_words("books/frankenstein.txt")} words found in the document")
+
 main()
