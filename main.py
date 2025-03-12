280f10505968e28e7d6a76f29dda1da7d80ef844
ngeorgiev92 <ngeorgiev92@gmail.com>
2025-03-12 13:16:55 +0000

    added code to read a text file and print to console stdout

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("greetings boots")
+def get_book_text(filedir):
+    with open(filedir) as f:
+        text = f.read()
+    return text
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+main()
